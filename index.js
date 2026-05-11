3a3a35b278c3b5df2d9ebbc5fd4f8cfbf834d5ef
Sebastián Vidaurri <seba.vidaurri@gmail.com>
2026-05-11 15:28:59 -0300

    fixed getproductById

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const apiService = {
     //metodo para obtener todos los productos
     async getAllProducts() {
         const response = await fetch(`${this.baseUrl}/products`);
+        if(!response.ok){
+            throw new Error (`Error ${response.status}: No se pudo obtener la lista de productos`);
+        }
         return response.json();
     },
 
     //metodo para obtener un producto por id
     async getProductById(id) {
         const response = await fetch(`${this.baseUrl}/products/${id}`);
+        if(!response.ok){
+            throw new Error (`Error ${response.status}: No se encontró el producto con ID ${id}`);
+        }
+        //revisamos que el tamaño del headers tenga contenido
+        //console.log(response) descubri que la API responde 200 aún si no existe el producto
+        const contentSize = response.headers.get('content-length');
+        if (contentSize === '0' || contentSize === null) {
+            throw new Error(`El producto con ID ${id} no existe (Respuesta vacía).`);
+        }
         return response.json();
     },
     
@@ -44,49 +56,58 @@ const apiService = {
 //iniciamos
 const args = process.argv.slice(2)
 //desestructuramos args par que se más legible 
-let [action, resource, id] = args;
+let [action, resource, ] = args;
+let id = undefined;
 
-if (!id){
+//si esta incluida un barra en resource
+if (resource && resource.includes("/")){
     [resource, id] = resource.split("/")
+    console.log(id);
 }
 
 //usamos una funsion async por que vamos a utilizar await apiService
 async function main() {
-    if (resource == "products"){
-            switch (action){
+    try{
+        if (resource == "products"){
+                switch (action){
 
-                case "GET":    
-                    if (!id){ //Si no hay ID entonces traemos todos los productos
-                        console.log("Lista de TODOS los productos: ");
-                        const productos = await apiService.getAllProducts();
-                        console.log(productos);
-                    } else { //Busqueda con id
-                        console.log(`Producto con ID ${id}: `);
-                        const producto = await apiService.getProductById(id);
-                        console.log(producto);
-                    };
-                    break;
+                    case "GET":
+                        const finalId = id || args[2];
+                        if (finalId) {
+                            const producto = await apiService.getProductById(finalId);
+                            console.log(`Producto con ID ${finalId}:`);
+                            console.log(producto);
+                        } else {
+                            console.log("Lista de TODOS los productos:");
+                            const productos = await apiService.getAllProducts();
+                            console.log(productos);
+                        }
+                        break;
 
-                case "POST":
-                    break;
+                    case "POST":
 
-                case "DELETE":
-                    if (id){
-                        console.log(`Eliminando el producto con id: ${id}...`)
-                        const borrado= await apiService.deleteProduct(id);
-                        console.log("Pruducto eliminado: ", borrado);
-                    } else {
-                        console.log("Error: Debes indicar un ID (ej: products/2)")
-                    }
-                    break;
+                        break;
 
-                default:
-                    console.log("Acción no reconocida. Usa GET, POST o DELETE")
+                    case "DELETE":
+                        if (id){
+                            console.log(`Eliminando el producto con id: ${id}...`)
+                            const borrado= await apiService.deleteProduct(id);
+                            console.log("Pruducto eliminado: ", borrado);
+                        } else {
+                             throw new Error (`Error: Debes indicar un ID (ej: products/2)`)
+                        }
+                        break;
+
+                    default:
+                        console.log("Acción no reconocida. Usa GET, POST o DELETE")
+            }
+
         }
-
+    } catch(error) {
+        // MOSTRAR EL ERROR POR PANTALLA
+        console.error("\x1b[31m%s\x1b[0m", " OCURRIÓ UN ERROR ");
+        console.error(`Detalle: ${error.message}`);
     }
-    
-    
 }
 
 main()
